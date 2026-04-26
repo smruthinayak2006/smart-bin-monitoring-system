735fd54b49574e7926d5410482eb405c5c474b6a
Smruthi Nayak <smritinayak2006@gmail.com>
2026-04-26 08:46:20 +0530

    Added simulated live telemetry updates to dashboard

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,1 +1,23 @@
-console.log("Dashboard loaded");
+function updateBars(){
+
+let fills =
+document.querySelectorAll(".fill");
+
+fills.forEach(function(bar){
+
+let value=
+Math.floor(
+60 + Math.random()*40
+);
+
+bar.style.width=
+value+"%";
+
+});
+
+}
+
+setInterval(
+updateBars,
+3000
+);
